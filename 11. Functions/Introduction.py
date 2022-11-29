e4c63bf4b4b5b439bebf18e8f9384cf3f534e95c
Yash Gujela <117921372+yashgujela98@users.noreply.github.com>
2022-11-29 19:21:11 +0530

    Add files via upload

---
+++ 11. Functions/Introduction.py
@@ -0,0 +1,37 @@
+'''
+Need of functions
+=================
+Reusability of code.
+
+What is function?
+=================
+Portion  or pieace of code defined once and called n number of
+times when required is called as Function.
+
+Types of functions
+==================
+1)Built in Function
+2)user defined function
+
+'''
+
+#sorted()
+
+l=[10,20,50,1,2,-3,-84]
+print(l)
+lsort=sorted(l)                     #asceding order
+lsortdesc=sorted(l,reverse=True)    #descending order
+print(lsort)
+print(lsortdesc)
+
+
+#pow(n,p): Used to calculate a number raised to another.
+
+r=pow(4,3)
+print("Result:",r)
+
+#sum(): Summation of list elements.
+l1=[10,20,54,8,5,-4,-5]
+s=sum(l1)
+print("Total:",s)
+
