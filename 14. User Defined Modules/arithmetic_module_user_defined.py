f2e8a5f461e63812bb819c597c5febf9dbb1dada
Yash Gujela <117921372+yashgujela98@users.noreply.github.com>
2022-12-12 19:30:00 +0530

    Add files via upload

---
+++ 14. User Defined Modules/arithmetic_module_user_defined.py
@@ -0,0 +1,27 @@
+'''
+User Defined Modules
+====================
+
+Arithmetic Module
+1)Addition 2)Subtractiom 3)Multiplication 4)Division
+'''
+
+def add(x,y):
+
+    z=x+y
+    return z
+
+def sub(x,y):
+
+    z=x+y
+    return z
+
+def mul(x,y):
+
+    z=x*y
+    return z
+
+def div(x,y):
+
+    z=x/y
+    return z
---
+++ 14. User Defined Modules/import_user_defined_module.py
@@ -0,0 +1,6 @@
+import arithmetic_module_user_defined as ar
+
+a=int(input("Enter first number:"))
+b=int(input("Enter second number:"))
+res=ar.add(a,b)
+print("Addition is:",res)
